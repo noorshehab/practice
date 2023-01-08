5506f2328db03ad6156f55c471551a1a1422bbdd
noorshehab <116682177+noorshehab@users.noreply.github.com>
2023-01-08 16:10:50 +0200

    Create digitsum.cpp

---
+++ digitsum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<math.h>
+using namespace  std;
+int wrapped(int n)
+{
+	if (n == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return wrapped(n/10)+ n%10;
+	}
+}
+
+
+int main()
+{
+	cout<<wrapped(1792)<<endl;
+	/*
+	for (int i = 0,j = 1792; i <=4; i++)
+	{
+		int x = pow(10, i);
+		
+		cout<<(j/x)%10<<endl;
+	}*/
+}
